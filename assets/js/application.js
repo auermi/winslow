deb84ecef42b0a67fdb876ab70bc13339207c187
Michael Auer <mauermcx@gmail.com>
2017-05-10 00:04:16 -0400

    Fixing broken javascript and moving css link back to head tag

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,11 +1,3 @@
-// Format Page Title
-if (window.location.pathname === '/') {
-  document.title = '{{ @blog.title }}';
-} else {
-  var metaTitle = '{{meta_title}}';
-  document.title = metaTitle.charAt(0).toUpperCase() + metaTitle.slice(1) - '{{@blog.title}}';
-}
-
 // Initialize mobile menu
 var menu = document.getElementById('menu');
 
