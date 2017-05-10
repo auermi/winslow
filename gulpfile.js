27b949b478d0dddc0b6bfee0203036e22fd55b8c
Michael Auer <mauermcx@gmail.com>
2017-05-09 23:37:32 -0400

    Minified js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var sass = require('gulp-sass');
 var csso = require('gulp-csso');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('css', function(){
   return gulp.src('assets/sass/style.scss')
@@ -14,10 +16,21 @@ gulp.task('css', function(){
     .pipe(gulp.dest('assets/css'))
 });
 
+gulp.task('js', () => {
+  return gulp.src('assets/js/application.js')
+    .pipe(uglify())
+    .pipe(rename('application.min.js'))
+    .pipe(gulp.dest('assets/js'))
+})
+
 gulp.watch('assets/sass/*.scss', ['css']).on('change', (e) => {
   console.log('File ' + e.path + ' was ' + e.type + ', transpiling Sass files...')
 });
 
-gulp.task('default', ['css'], () => {
+gulp.watch('assets/js/*.js', ['js']).on('change', (e) => {
+  console.log('File ' + e.path + ' was ' + e.type + ', minifying javascript files...')
+});
+
+gulp.task('default', ['css', 'js'], () => {
   console.log('\n\n===\n\nGulp is Running 🥃\n\n==\n\n')
 });
